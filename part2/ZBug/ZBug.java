4f613e1e840e72c6f24072f38e7329809570d84a
saltydong <54834885+saltydong@users.noreply.github.com>
2019-11-17 14:40:40 +0800

    1

---
+++ part2/ZBug/ZBug.java
@@ -0,0 +1,77 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ */
+
+import info.gridworld.actor.Bug;
+
+/**
+ * A <code>ZBug</code> traces out a square "box" of a given size. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ZBug extends Bug
+{
+    private int steps;
+    private int sideLength;
+    private int t;
+    /**
+     * Constructs a box bug that traces a square of a given side length
+     * @param length the side length
+     */
+    public ZBug(int length)
+    {
+        steps = 0;
+        sideLength = length;
+        t = 0; // use t to make a z
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    public void act()
+    {
+        if (steps < sideLength && canMove())
+        {
+            move();
+            steps++;
+        }
+        else
+        {
+            if(t == 0)
+            {
+                turn();
+                turn();
+                turn();
+                steps = 0;
+                t++;
+            }
+            else if(t == 1)
+            {
+                turn();
+                turn();
+                turn();
+                turn();
+                turn();
+                steps = 0;
+                t++;
+            }
+            else if(t == 2)
+            {
+                sideLength = 0;
+            }
+        }
+    }
+}
